b6846770a4e7c3d67b1cd38e64c016a1e6272eeb
Mostavluk A <most96@i.ua>
2017-10-10 21:33:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,54 @@
+$(function(){
+
+	// Filter buttons
+	$('.filter-bar-element a').on('click', function(e) {
+		e.preventDefault();
+		var self = $(this);
+		var elToShow = self.parents('.filter-bar-element').attr('rel');
+		if(elToShow == '.all'){
+			$('.gallery-element').show(300);
+		}
+		else{
+			$('.gallery-element').hide(300);
+			$(elToShow).show(300);
+		}
+	});
+
+	// Menu button 
+	$('.menu a, .close-menu').on('click', function(e) {
+		e.preventDefault();
+		$('.open-menu').toggleClass('hiden');
+		$('.open-menu').addClass('animated fadeIn');
+		$('.menu a').fadeToggle(300);
+		$('.search a').fadeToggle(300);
+	});
+
+	// Search button 
+	$('.search a, .close-search').on('click', function(e) {
+		e.preventDefault();
+		$('.open-search').toggleClass('hiden');
+		$('.open-search').addClass('animated fadeIn');
+		$('.search a').fadeToggle(300);
+		$('.menu a').fadeToggle(300);
+	});
+
+	// Show picture page
+	$('.gallery-element a').on('click', function(e) {
+		e.preventDefault();
+		var self = $(this);
+		var name = self.data('name');
+		var artist = self.data('artist');
+		var price = self.data('price');
+		var img = self.data('img');
+		$('.content .filter-bar, .content .gallery').fadeToggle(300);
+		$('.picture').delay(300).fadeToggle(300);
+		$('.picture img').attr('src', img);
+		$('.picture h3 span').text(name);
+		$('.picture p:nth-child(2) span').text(artist);
+		$('.picture p:nth-child(3) span').text(price);
+	});
+
+});
+
+
+
